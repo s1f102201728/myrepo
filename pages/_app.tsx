a4908adb8fcf2b9186e4113784638cd247522569
konjikun <s1f102200378@iniad.org>
2022-11-13 11:29:27 +0900

    appのエラー修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import React from 'react'
-import { AppProps } from 'next/app'
+import type { AppProps } from 'next/app'
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
